8548ab67fd1523e07495e06e16e70dd08da03f38
naoufal-emsi <naoufal.elouahabi@emsi-edu.ma>
2025-12-20 11:51:30 +0100

    refactor : deleted old activity_doctor_records and added new one

--- app/src/main/java/m/health/DossiersMedicaux.java
+++ app/src/main/java/m/health/DossiersMedicaux.java
@@ -8,6 +8,6 @@ public class DossiersMedicaux extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dossiers_medicaux);
+        setContentView(R.layout.activity_doctor_records);
     }
 }
